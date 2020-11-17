de1c10ef02142e3e23edda67b5528b1450c1051c
rilangki <rilangkishabong@gmail.com>
2020-11-17 11:26:42 +0530

    applied authentication, created middlewares and refactored code for connection to server.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,32 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
-import { router as clanRouter } from './resources/clan/router';
+import clanRouter from './resources/clan/router';
 import userRouter from './resources/user/router';
+import { signin, signup, protect } from './utils/auth';
+import cors from 'cors';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
+const options = {
+  key: fs.readFileSync(__dirname + '/cert/private.pem'),
+  cert: fs.readFileSync(__dirname + '/cert/server.cert'),
+};
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/user', userRouter);
-app.use('/clan', clanRouter);
+app.use((req, res, next) => {
+  console.log(req.body);
+  next();
+});
+app.use('/api', protect);
+app.use('/api/user', userRouter);
+app.use('/api/clan', clanRouter);
+app.post('/signin', signin);
+app.post('/signup', signup);
 
 // const db = 'mongodb+srv://Heace:heace@cluster0.p0yf3.mongodb.net/test';
 const db = require('./config/keys').mongoURI;
@@ -20,13 +36,22 @@ const opts = {
   user: process.env.DB_USERNAME,
   pass: process.env.DB_PASSWORD,
 };
-mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true, ...opts })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch((err) => console.log(err));
+const httpsServer = https.createServer(options, app);
 
-const port = process.env.PORT;
+const start = async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ...opts,
+    });
 
-app.listen(port, () => {
-  console.log(`app is listening to port ${port}`);
-});
+    httpsServer.listen(process.env.PORT, () => {
+      console.log(`app is listening to port ${process.env.PORT}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+start();
