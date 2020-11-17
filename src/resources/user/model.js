b5efed8538781e8199afa31c669db69a9bc53a62
rilangki <rilangkishabong@gmail.com>
2020-11-17 11:43:08 +0530

    created schema for user.

--- src/resources/user/model.js
+++ src/resources/user/model.js
@@ -1,8 +1,43 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    this.password = await bcrypt.hash(this.password, 8);
+    next();
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+userSchema.methods.checkPassword = function (password) {
+  const passwordHash = this.password;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(same);
+    });
+  });
+};
 
 export const User = mongoose.model('user', userSchema);
